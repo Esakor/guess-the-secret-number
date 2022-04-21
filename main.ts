76ffe4cecd6bc1031f2f5894ca2502fdf4ece262
Esakor <100299697+Esakor@users.noreply.github.com>
2022-04-21 14:33:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,20 +5,26 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (number > Random) {
         basic.showString("High")
+        Guesses += 1
     } else if (number < Random) {
         basic.showString("Low")
+        Guesses += 1
     } else if (number == Random) {
         basic.showString("Win")
+        basic.showString("Guesses")
+        basic.showNumber(Guesses)
     }
 })
 input.onButtonPressed(Button.B, function () {
     number += -1
     basic.showNumber(number)
 })
+let Guesses = 0
 let number = 0
 let Random = 0
 Random = randint(0, 20)
 number = 0
+Guesses = 0
 basic.forever(function () {
 	
 })
